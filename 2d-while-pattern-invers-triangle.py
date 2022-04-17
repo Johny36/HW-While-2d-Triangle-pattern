f13df5428749ac80bf8774c808515e455563ccf0
Ion G <98644414+Johny36@users.noreply.github.com>
2022-04-17 15:45:20 +0300

    Add files via upload

---
+++ 2d-while-pattern-invers-triangle.py
@@ -0,0 +1,24 @@
+# HW1: 5x5 Drawing THIS
+# *        
+# * *        
+# * * *     
+# * * * *
+# * * * * *
+
+print("\n")
+
+
+# #### DRAW 5 stars ###
+
+s = 1
+
+while s <= 5:
+    j = 5
+    while j >= s:
+        print("*", end=" ")
+        j -= 1
+    print()
+    s += 1
+
+
+print("\n")
