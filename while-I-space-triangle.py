626a31ca52ef1e7a0eb2731a7e259fefcccae4cf
Ion G <98644414+Johny36@users.noreply.github.com>
2022-04-25 14:42:09 +0300

    Add files via upload

---
+++ while-I-space-triangle.py
@@ -0,0 +1,29 @@
+# HW1: 5x5 Drawing THIS
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+
+rows = 8
+num = 1
+
+while num <= rows:
+    space = num
+#   printing space before stars
+    while space < rows:
+        print(" ", end =" ")
+        space += 1
+    l = 1
+    # print stars
+    while l <= num:
+        print("*", end =" ")
+        l += 1
+    print()
+    num += 1
+
+   
+
+
+
+    
